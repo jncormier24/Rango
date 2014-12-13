233873a3aa5229746a7d87c8df368247efb1765d
Zach Tirrell <zbtirrell@gmail.com>
2014-12-12 23:52:37 -0500

    - created a new app: rango - created a view for rango - added rango views to tango urls

--- tango_with_django_project/rango/urls.py
+++ tango_with_django_project/rango/urls.py
@@ -1,19 +1,19 @@
 from django.conf.urls import patterns, include, url
+from rango import views
 
 # Uncomment the next two lines to enable the admin:
-# from django.contrib import admin
-# admin.autodiscover()
+from django.contrib import admin
+admin.autodiscover()
 
 urlpatterns = patterns('',
+	url( r'^$', views.index, name='index' ),
     # Examples:
     # url(r'^$', 'tango_with_django_project.views.home', name='home'),
     # url(r'^tango_with_django_project/', include('tango_with_django_project.foo.urls')),
 
     # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+    url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
     # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
-
-    url( r'^rango/', include( 'rango.urls' ) )
+    url(r'^admin/', include(admin.site.urls)),
 )
